1ad8482f8f90369fa1c9718b04c7a685f3a9e98e
Tess <tess@person.com>
2021-12-19 22:09:07 -0600

    Refactor DependencyInjection

--- PuppyPlace.Console/DependencyInjection.cs
+++ PuppyPlace.Console/DependencyInjection.cs
@@ -1,12 +1,16 @@
 using PuppyPlace.Data;
+using PuppyPlace.Repository;
 
 namespace PuppyPlace.Console;
 
 public static class DependencyInjection
 {
-    public static PersonTools PersonTools = new PersonTools(new Prompt(), new PuppyPlaceContext());
+    public static PersonTools PersonTools = new PersonTools(new Prompt(), 
+        new PersonRepository(new PuppyPlaceContext()), 
+        new DogRepository(new PuppyPlaceContext()), 
+        new AdoptionService(new PuppyPlaceContext()));
     
-    public static DogTools DogTools = new DogTools(new Prompt(), new PuppyPlaceContext());
+    public static DogTools DogTools = new DogTools(new Prompt(), new DogRepository(new PuppyPlaceContext()), new PersonRepository(new PuppyPlaceContext()));
     
     public static PuppyPlaceContext _PuppyPlaceContext = new PuppyPlaceContext();
     
