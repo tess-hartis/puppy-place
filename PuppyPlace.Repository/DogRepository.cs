276172cbf3414807729a283f5345fb87004b6ed1
Tess <tess@person.com>
2021-12-19 22:09:38 -0600

    Create DogRepository and add methods

--- PuppyPlace.Repository/DogRepository.cs
+++ PuppyPlace.Repository/DogRepository.cs
@@ -1,15 +1,43 @@
-﻿using PuppyPlace.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using PuppyPlace.Data;
 using PuppyPlace.Domain;
 
 namespace PuppyPlace.Repository;
 
 public class DogRepository
 {
-    private PuppyPlaceContext _context = new PuppyPlaceContext();
+    private PuppyPlaceContext _context;
+    
+    public DogRepository(PuppyPlaceContext context)
+    {
+        _context = context;
+    }
 
     public void AddDog(Dog dog)
     {
         _context.Dogs.Add(dog);
+        _context.SaveChanges();
+    }
+
+    public IReadOnlyList<Dog> Dogs()
+    {
+        return _context.Dogs.ToList();
+    }
+
+    public Dog? FindById(Guid id)
+    {
+        return _context.Dogs.Include(d => d.Owner).FirstOrDefault(d => d.Id == id);
+    }
+
+    public void DeleteDog(Dog dog)
+    {
+        _context.Dogs.Remove(dog);
+        _context.SaveChanges();
+    }
+
+    public void UpdateName(Dog dog)
+    {
+        _context.Dogs.Update(dog);
         _context.SaveChanges();
     }
 }
