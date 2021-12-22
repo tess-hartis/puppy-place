6e0ed26f6a899badb013bd2a50803a3eaac5ef3a
Tess <tess@person.com>
2021-12-22 09:17:27 -0600

    migration files

---
+++ PuppyPlace.Data/Migrations/20211220043905_removeownerid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PuppyPlace.Data.Migrations
+{
+    public partial class removeownerid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Dogs");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "OwnerId",
+                table: "Dogs",
+                type: "uuid",
+                nullable: true);
+        }
+    }
+}
