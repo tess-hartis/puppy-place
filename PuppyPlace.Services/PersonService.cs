a9841608d593403cd97e73d9b8b19a5bcfbc5f6e
Tess <tess@person.com>
2021-12-30 13:11:09 -0600

    Add PersonService class to Services project

--- PuppyPlace.Services/PersonService.cs
+++ PuppyPlace.Services/PersonService.cs
@@ -1,5 +1,31 @@
-﻿namespace PuppyPlace.Services;
+﻿using FluentValidation.Results;
+using PuppyPlace.Domain;
+using PuppyPlace.Repository;
+
+namespace PuppyPlace.Services;
 
 public class PersonService
 {
+    private readonly PersonRepository _repository;
+    private readonly PersonValidator _validator;
+
+    public PersonService(PersonRepository repository, PersonValidator validator)
+    {
+        _repository = repository;
+        _validator = validator;
+    }
+
+    public async Task Validate(Person person)
+    {
+        ValidationResult results = _validator.Validate(person);
+
+        if (results.IsValid)
+        {
+            await _repository.AddPersonAsync(person);
+        }
+        else
+        {
+            throw new Exception("Not valid");
+        }
+    }
 }
