0367d29c9cd7a6085044a4bf90d44de6210f6a2d
tess-hartis <tess15hartis@gmail.com>
2022-02-19 11:26:18 -0600

    Create GetPersonByIdQueryHandler.cs

--- PuppyPlace.CqrsService/PersonCQ/Queries/GetPersonByIdQueryHandler.cs
+++ PuppyPlace.CqrsService/PersonCQ/Queries/GetPersonByIdQueryHandler.cs
@@ -1,6 +1,32 @@
+using LanguageExt;
+using MediatR;
+using PuppyPlace.Domain;
+using PuppyPlace.Repository;
+
 namespace PuppyPlace.CqrsService.PersonCQ.Queries;
 
-public class GetPersonByIdQueryHandler
+public class GetPersonByIdQuery : IRequest<Option<Person>>
 {
-    
+    public Guid Id { get; }
+
+    public GetPersonByIdQuery(Guid id)
+    {
+        Id = id;
+    }
+}
+public class GetPersonByIdQueryHandler : IRequestHandler<GetPersonByIdQuery, Option<Person>>
+{
+    private readonly IPersonRepository _personRepository;
+
+    public GetPersonByIdQueryHandler(IPersonRepository personRepository)
+    {
+        _personRepository = personRepository;
+    }
+
+    public async Task<Option<Person>> Handle
+        (GetPersonByIdQuery request, CancellationToken cancellationToken)
+    {
+        return await _personRepository.FindAsync(request.Id);
+    }
+
 }
