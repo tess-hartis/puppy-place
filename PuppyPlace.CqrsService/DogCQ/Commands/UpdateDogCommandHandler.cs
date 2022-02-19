810e3300b43420aee56989887c5ade100c5d2b6a
tess-hartis <tess15hartis@gmail.com>
2022-02-19 13:06:13 -0600

    Add UpdateDog and UpdatePerson command handlers

--- PuppyPlace.CqrsService/DogCQ/Commands/UpdateDogCommandHandler.cs
+++ PuppyPlace.CqrsService/DogCQ/Commands/UpdateDogCommandHandler.cs
@@ -1,6 +1,58 @@
+using LanguageExt;
+using LanguageExt.Common;
+using static LanguageExt.Prelude;
+using MediatR;
+using PuppyPlace.Domain;
+using PuppyPlace.Domain.ValueObjects.DogValueObjects;
+using PuppyPlace.Repository;
+using Unit = LanguageExt.Unit;
+
+
 namespace PuppyPlace.CqrsService.DogCQ.Commands;
 
-public class UpdateDogCommandHandler
+public class UpdateDogCommand : IRequest<Option<Validation<Error,Unit >>>
 {
-    
+    public Guid Id { get; set; }
+    public string Name { get; }
+    public int Age { get; }
+    public string Breed { get; }
+
+    public UpdateDogCommand(Guid id, string name, int age, string breed)
+    {
+        Id = id;
+        Name = name;
+        Age = age;
+        Breed = breed;
+    }
+}
+public class UpdateDogCommandHandler : 
+    IRequestHandler<UpdateDogCommand, Option<Validation<Error, Unit>>>
+{
+    private readonly IDogRepository _dogRepository;
+
+    public UpdateDogCommandHandler(IDogRepository dogRepository)
+    {
+        _dogRepository = dogRepository;
+    }
+
+    public async Task<Option<Validation<Error, Unit>>> Handle
+        (UpdateDogCommand command, CancellationToken cancellationToken)
+    {
+        var dog = await _dogRepository.FindAsync(command.Id);
+        var name = DogName.Create(command.Name);
+        var age = DogAge.Create(command.Age);
+        var breed = DogBreed.Create(command.Breed);
+
+        var updatedDog = dog
+            .Map(d => (name, age, breed)
+                .Apply(d.Update));
+
+        ignore(updatedDog
+            .Map(d =>
+                d.Map(async d => await _dogRepository.SaveAsync())));
+
+        return updatedDog;
+
+    }
+
 }
--- PuppyPlace.CqrsService/PersonCQ/Commands/UpdatePersonCommandHandler.cs
+++ PuppyPlace.CqrsService/PersonCQ/Commands/UpdatePersonCommandHandler.cs
@@ -1,6 +1,49 @@
+using LanguageExt;
+using LanguageExt.Common;
+using static LanguageExt.Prelude;
+using MediatR;
+using PuppyPlace.Domain;
+using PuppyPlace.Domain.ValueObjects.PersonValueObjects;
+using PuppyPlace.Repository;
+using Unit = LanguageExt.Unit;
+
 namespace PuppyPlace.CqrsService.PersonCQ.Commands;
 
-public class UpdatePersonCommandHandler
+public class UpdatePersonCommand : IRequest<Option<Validation<Error, Unit>>>
 {
-    
+    public Guid Id { get; set; }
+    public string Name { get; }
+
+    public UpdatePersonCommand(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
+public class UpdatePersonCommandHandler : 
+    IRequestHandler<UpdatePersonCommand, Option<Validation<Error, Unit>>>
+{
+    private readonly IPersonRepository _personRepository;
+
+    public UpdatePersonCommandHandler(IPersonRepository personRepository)
+    {
+        _personRepository = personRepository;
+    }
+
+    public async Task<Option<Validation<Error, Unit>>> Handle
+        (UpdatePersonCommand command, CancellationToken cancellationToken)
+    {
+        var person = await _personRepository.FindAsync(command.Id);
+        var name = PersonName.Create(command.Name);
+
+        var updatedPerson = person
+            .Map(p => name.Map(p.Update));
+
+        ignore(updatedPerson
+            .Map(p =>
+                p.Map(async x => await _personRepository.SaveAsync())));
+
+        return updatedPerson;
+
+    }
 }
