668fbe91db673c3e224592442de8f91b8e49d6e4
tess-hartis <tess15hartis@gmail.com>
2022-02-19 12:35:17 -0600

    Create DeleteDog and DeletePerson command handlers

--- PuppyPlace.CqrsService/DogCQ/Commands/DeleteDogCommandHandler.cs
+++ PuppyPlace.CqrsService/DogCQ/Commands/DeleteDogCommandHandler.cs
@@ -1,6 +1,35 @@
+using LanguageExt;
+using static LanguageExt.Prelude;
+using MediatR;
+using PuppyPlace.Repository;
+using Unit = LanguageExt.Unit;
+
 namespace PuppyPlace.CqrsService.DogCQ.Commands;
 
-public class DeleteDogCommandHandler
+public class DeleteDogCommand : IRequest<Option<Unit>>
 {
-    
+    public Guid Id;
+
+    public DeleteDogCommand(Guid id)
+    {
+        Id = id;
+    }
+}
+public class DeleteDogCommandHandler : IRequestHandler<DeleteDogCommand, Option<Unit>>
+{
+    private readonly IDogRepository _dogRepository;
+
+    public DeleteDogCommandHandler(IDogRepository dogRepository)
+    {
+        _dogRepository = dogRepository;
+    }
+
+    public async Task<Option<Unit>> Handle
+        (DeleteDogCommand command, CancellationToken cancellationToken)
+    {
+        var dog = await _dogRepository.FindAsync(command.Id);
+        ignore(dog.Map(async d => await _dogRepository.DeleteAsync(d)));
+        return dog.Map(d => unit);
+
+    }
 }
--- PuppyPlace.CqrsService/PersonCQ/Commands/DeletePersonCommandHandler.cs
+++ PuppyPlace.CqrsService/PersonCQ/Commands/DeletePersonCommandHandler.cs
@@ -1,6 +1,34 @@
+using LanguageExt;
+using static LanguageExt.Prelude;
+using MediatR;
+using PuppyPlace.Repository;
+using Unit = LanguageExt.Unit;
+
 namespace PuppyPlace.CqrsService.PersonCQ.Commands;
 
-public class DeletePersonCommandHandler
+public class DeletePersonCommand : IRequest<Option<Unit>>
 {
-    
+    public Guid Id;
+
+    public DeletePersonCommand(Guid id)
+    {
+        Id = id;
+    }
+}
+public class DeletePersonCommandHandler : IRequestHandler<DeletePersonCommand, Option<Unit>>
+{
+    private readonly IPersonRepository _personRepository;
+
+    public DeletePersonCommandHandler(IPersonRepository personRepository)
+    {
+        _personRepository = personRepository;
+    }
+
+    public async Task<Option<Unit>> Handle
+        (DeletePersonCommand command, CancellationToken cancellationToken)
+    {
+        var person = await _personRepository.FindAsync(command.Id);
+        ignore(person.Map(async p => await _personRepository.DeleteAsync(p)));
+        return person.Map(p => unit);
+    }
 }
