71ffef6fb064bdd300618852c2f5fafc5bcd193c
tess-hartis <tess15hartis@gmail.com>
2022-02-19 11:05:26 -0600

    Comment out Person and Dog Services

--- PuppyPlace.Services/DogService.cs
+++ PuppyPlace.Services/DogService.cs
@@ -1,47 +1,47 @@
-using System.ComponentModel.DataAnnotations;
-using FluentValidation;
-using PuppyPlace.Domain;
-using PuppyPlace.Repository;
-using ValidationResult = FluentValidation.Results.ValidationResult;
-
-namespace PuppyPlace.Services;
-
-public class DogService
-{
-    private readonly IDogRepository _repository;
-    private readonly DogValidator _validator;
-
-    public DogService(IDogRepository repository, DogValidator validator)
-    {
-        _repository = repository;
-        _validator = validator;
-    }
-
-    public async Task ValidateDog(Dog dog)
-    {
-        ValidationResult results = _validator.Validate(dog);
-
-        if (results.IsValid)
-        {
-            await _repository.AddDogAsync(dog);
-        }
-        else
-        {
-            throw new Exception("Not valid");
-        }
-    }
-
-    public async Task ValidateUpdatedDog(Dog dog)
-    {
-        ValidationResult results = _validator.Validate(dog);
-
-        if (results.IsValid)
-        {
-            await _repository.UpdateNameAsync(dog);
-        }
-        else
-        {
-            throw new Exception("Not valid");
-        }
-    }
-}
+// using System.ComponentModel.DataAnnotations;
+// using FluentValidation;
+// using PuppyPlace.Domain;
+// using PuppyPlace.Repository;
+// using ValidationResult = FluentValidation.Results.ValidationResult;
+//
+// namespace PuppyPlace.Services;
+//
+// public class DogService
+// {
+//     private readonly IDogRepository _repository;
+//     private readonly DogValidator _validator;
+//
+//     public DogService(IDogRepository repository, DogValidator validator)
+//     {
+//         _repository = repository;
+//         _validator = validator;
+//     }
+//
+//     public async Task ValidateDog(Dog dog)
+//     {
+//         ValidationResult results = _validator.Validate(dog);
+//
+//         if (results.IsValid)
+//         {
+//             await _repository.AddDogAsync(dog);
+//         }
+//         else
+//         {
+//             throw new Exception("Not valid");
+//         }
+//     }
+//
+//     public async Task ValidateUpdatedDog(Dog dog)
+//     {
+//         ValidationResult results = _validator.Validate(dog);
+//
+//         if (results.IsValid)
+//         {
+//             await _repository.UpdateNameAsync(dog);
+//         }
+//         else
+//         {
+//             throw new Exception("Not valid");
+//         }
+//     }
+// }
--- PuppyPlace.Services/PersonService.cs
+++ PuppyPlace.Services/PersonService.cs
@@ -1,33 +1,33 @@
-﻿using FluentValidation.Results;
-using PuppyPlace.Domain;
-using PuppyPlace.Repository;
-
-namespace PuppyPlace.Services;
-
-public class PersonService
-{
-    private readonly IPersonRepository _repository;
-    private readonly PersonValidator _validator;
-
-    public PersonService(IPersonRepository repository, PersonValidator validator)
-    {
-        _repository = repository;
-        _validator = validator;
-    }
-
-
-    public async Task ValidateNewPerson(Person person)
-    {
-        ValidationResult results = _validator.Validate(person);
-
-        if (results.IsValid)
-        {
-            await _repository.AddPersonAsync(person);
-        }
-        else
-        {
-            throw new Exception("Not valid");
-        }
-    }
-    
-}
+﻿// using FluentValidation.Results;
+// using PuppyPlace.Domain;
+// using PuppyPlace.Repository;
+//
+// namespace PuppyPlace.Services;
+//
+// public class PersonService
+// {
+//     private readonly IPersonRepository _repository;
+//     private readonly PersonValidator _validator;
+//
+//     public PersonService(IPersonRepository repository, PersonValidator validator)
+//     {
+//         _repository = repository;
+//         _validator = validator;
+//     }
+//
+//
+//     public async Task ValidateNewPerson(Person person)
+//     {
+//         ValidationResult results = _validator.Validate(person);
+//
+//         if (results.IsValid)
+//         {
+//             await _repository.AddPersonAsync(person);
+//         }
+//         else
+//         {
+//             throw new Exception("Not valid");
+//         }
+//     }
+//     
+// }
