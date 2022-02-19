d6cec76b27b9343fa2e6e2642d3357b0ac2dd142
tess-hartis <tess15hartis@gmail.com>
2022-02-19 11:00:17 -0600

    Comment out fluent validators

--- PuppyPlace.Domain/DogValidator.cs
+++ PuppyPlace.Domain/DogValidator.cs
@@ -1,43 +1,43 @@
-using System.Data;
-using FluentValidation;
-
-namespace PuppyPlace.Domain;
-
-public class DogValidator : AbstractValidator<Dog>
-{
-    public DogValidator()
-    {
-        RuleFor(d => d.Name)
-            .Cascade(CascadeMode.Stop)
-            .NotEmpty().WithMessage("cannot be empty")
-            .Length(2, 20).WithMessage("must be between 2 and 20 characters")
-            .Must(BeValidName).WithMessage("Cannot contain special characters");
-        RuleFor(d => d.Breed)
-            .Cascade(CascadeMode.Stop)
-            .NotEmpty().WithMessage("cannot be empty")
-            .Length(2, 20).WithMessage("must be between 2 and 20 characters")
-            .Must(BeValidName).WithMessage("Cannot contain special characters");
-        RuleFor(d => d.Age)
-            .Cascade(CascadeMode.Stop)
-            .NotEmpty().WithMessage("Age cannot be empty")
-            .Must(BeValidAge).WithMessage("Age must be between 0 and 25");
-    }
-    
-    private bool BeValidName(string name)
-    {
-        name = name.Replace(" ", "");
-        name = name.Replace("-", "");
-        return name.All(Char.IsLetter);
-    }
-
-    private bool BeValidAge(int age)
-    {
-        if (age >= 0 && age <= 25)
-        {
-            return true;
-        }
-        
-        return false;
-    }
-    
-}
+// using System.Data;
+// using FluentValidation;
+//
+// namespace PuppyPlace.Domain;
+//
+// public class DogValidator : AbstractValidator<Dog>
+// {
+//     public DogValidator()
+//     {
+//         RuleFor(d => d.Name)
+//             .Cascade(CascadeMode.Stop)
+//             .NotEmpty().WithMessage("cannot be empty")
+//             .Length(2, 20).WithMessage("must be between 2 and 20 characters")
+//             .Must(BeValidName).WithMessage("Cannot contain special characters");
+//         RuleFor(d => d.Breed)
+//             .Cascade(CascadeMode.Stop)
+//             .NotEmpty().WithMessage("cannot be empty")
+//             .Length(2, 20).WithMessage("must be between 2 and 20 characters")
+//             .Must(BeValidName).WithMessage("Cannot contain special characters");
+//         RuleFor(d => d.Age)
+//             .Cascade(CascadeMode.Stop)
+//             .NotEmpty().WithMessage("Age cannot be empty")
+//             .Must(BeValidAge).WithMessage("Age must be between 0 and 25");
+//     }
+//     
+//     private bool BeValidName(string name)
+//     {
+//         name = name.Replace(" ", "");
+//         name = name.Replace("-", "");
+//         return name.All(Char.IsLetter);
+//     }
+//
+//     private bool BeValidAge(int age)
+//     {
+//         if (age >= 0 && age <= 25)
+//         {
+//             return true;
+//         }
+//         
+//         return false;
+//     }
+//     
+// }
--- PuppyPlace.Domain/PersonValidator.cs
+++ PuppyPlace.Domain/PersonValidator.cs
@@ -1,23 +1,23 @@
-using FluentValidation;
-
-namespace PuppyPlace.Domain;
-
-public class PersonValidator : AbstractValidator<Person>
-{
-    public PersonValidator()
-    {
-        RuleFor(p => p.Name)
-            .Cascade(CascadeMode.Stop)
-            .NotEmpty().WithMessage("cannot be empty")
-            .Length(2, 20).WithMessage("must be between 2 and 20 characters")
-            .Must(BeValidName).WithMessage("Cannot contain special characters");
-
-    }
-
-    private bool BeValidName(string name)
-    {
-        name = name.Replace(" ", "");
-        name = name.Replace("-", "");
-        return name.All(Char.IsLetter);
-    }
-}
+// using FluentValidation;
+//
+// namespace PuppyPlace.Domain;
+//
+// public class PersonValidator : AbstractValidator<Person>
+// {
+//     public PersonValidator()
+//     {
+//         RuleFor(p => p.Name)
+//             .Cascade(CascadeMode.Stop)
+//             .NotEmpty().WithMessage("cannot be empty")
+//             .Length(2, 20).WithMessage("must be between 2 and 20 characters")
+//             .Must(BeValidName).WithMessage("Cannot contain special characters");
+//
+//     }
+//
+//     private bool BeValidName(string name)
+//     {
+//         name = name.Replace(" ", "");
+//         name = name.Replace("-", "");
+//         return name.All(Char.IsLetter);
+//     }
+// }
