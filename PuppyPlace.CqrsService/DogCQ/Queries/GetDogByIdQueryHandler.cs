bcced9c1cc5ef345ab36ca0615057398bd3cf16d
tess-hartis <tess15hartis@gmail.com>
2022-02-19 11:37:16 -0600

    Add GetDogByIdQueryHandler.cs

--- PuppyPlace.CqrsService/DogCQ/Queries/GetDogByIdQueryHandler.cs
+++ PuppyPlace.CqrsService/DogCQ/Queries/GetDogByIdQueryHandler.cs
@@ -1,6 +1,32 @@
+using LanguageExt;
+using MediatR;
+using PuppyPlace.Domain;
+using PuppyPlace.Repository;
+
 namespace PuppyPlace.CqrsService.DogCQ.Queries;
 
-public class GetDogByIdQueryHandler
+public class GetDogByIdQuery : IRequest<Option<Dog>>
 {
-    
+    public Guid Id { get; }
+
+    public GetDogByIdQuery(Guid id)
+    {
+        Id = id;
+    }
+}
+public class GetDogByIdQueryHandler : 
+    IRequestHandler<GetDogByIdQuery, Option<Dog>>
+{
+    private readonly IDogRepository _dogRepository;
+
+    public GetDogByIdQueryHandler(IDogRepository dogRepository)
+    {
+        _dogRepository = dogRepository;
+    }
+
+    public async Task<Option<Dog>> Handle
+        (GetDogByIdQuery request, CancellationToken cancellationToken)
+    {
+        return await _dogRepository.FindAsync(request.Id);
+    }
 }
