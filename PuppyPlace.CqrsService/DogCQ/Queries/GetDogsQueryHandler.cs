b47f4068c95754c27f8637ca6744970df7592ada
tess-hartis <tess15hartis@gmail.com>
2022-02-19 11:29:10 -0600

    Add GetDogsQueryHandler.cs

--- PuppyPlace.CqrsService/DogCQ/Queries/GetDogsQueryHandler.cs
+++ PuppyPlace.CqrsService/DogCQ/Queries/GetDogsQueryHandler.cs
@@ -1,6 +1,25 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PuppyPlace.Domain;
+using PuppyPlace.Repository;
+
 namespace PuppyPlace.CqrsService.DogCQ.Queries;
 
-public class GetDogsQueryHandler
+public record GetDogsQuery : IRequest<IEnumerable<Dog>>;
+
+public class GetDogsQueryHandler : IRequestHandler<GetDogsQuery, IEnumerable<Dog>>
 {
-    
+    private readonly IDogRepository _dogRepository;
+
+    public GetDogsQueryHandler(IDogRepository dogRepository)
+    {
+        _dogRepository = dogRepository;
+    }
+
+    public async Task<IEnumerable<Dog>> Handle
+        (GetDogsQuery request, CancellationToken cancellationToken)
+    {
+        var dogs = await _dogRepository.GetEntities().ToListAsync();
+        return dogs;
+    }
 }
