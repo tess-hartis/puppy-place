574c217d399389f0daf87e031fa0b2823d29ef51
tess-hartis <tess15hartis@gmail.com>
2022-02-19 12:26:50 -0600

    Create AddDogCommandHandler.cs

--- PuppyPlace.CqrsService/DogCQ/Commands/AddDogCommandHandler.cs
+++ PuppyPlace.CqrsService/DogCQ/Commands/AddDogCommandHandler.cs
@@ -1,6 +1,51 @@
+using LanguageExt;
+using LanguageExt.Common;
+using MediatR;
+using PuppyPlace.Domain;
+using PuppyPlace.Domain.ValueObjects.DogValueObjects;
+using PuppyPlace.Repository;
+
 namespace PuppyPlace.CqrsService.DogCQ.Commands;
 
-public class AddDogCommandHandler
+public class AddDogCommand : IRequest<Validation<Error, Dog>>
 {
-    
+    public string Name { get; }
+    public int Age { get; }
+    public string Breed { get; }
+
+    public AddDogCommand(string name, int age, string breed)
+    {
+        Name = name;
+        Age = age;
+        Breed = breed;
+    }
+}
+public class AddDogCommandHandler : IRequestHandler<AddDogCommand, Validation<Error, Dog>>
+{
+    private readonly IDogRepository _dogRepository;
+
+    public AddDogCommandHandler(IDogRepository dogRepository)
+    {
+        _dogRepository = dogRepository;
+    }
+
+    public async Task<Validation<Error, Dog>> Handle
+        (AddDogCommand command, CancellationToken cancellationToken)
+    {
+        var name = DogName.Create(command.Name);
+        var age = DogAge.Create(command.Age);
+        var breed = DogBreed.Create(command.Breed);
+
+        var newDog = (name, age, breed)
+            .Apply(Dog.Create);
+
+        await newDog
+            .Succ(async d =>
+            {
+                await _dogRepository.AddAsync(d);
+            })
+            .Fail(e => e.AsTask());
+
+        return newDog;
+    }
 }
