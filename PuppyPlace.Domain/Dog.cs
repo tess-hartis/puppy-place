e36aeb2236feb2ec98df3929c2915ef44ad12a5e
tess-hartis <tess15hartis@gmail.com>
2022-02-19 10:36:19 -0600

    Update Dog and Person models to use value objects as properties

---
+++ PuppyPlace.Domain/Dog.cs
@@ -0,0 +1,40 @@
+using LanguageExt;
+using PuppyPlace.Domain.ValueObjects.DogValueObjects;
+
+namespace PuppyPlace.Domain;
+
+public class Dog
+{
+    public Guid Id { get; set; }
+    public DogName Name { get; set; }
+    public DogAge Age { get; set; }
+    public DogBreed Breed { get; set; }
+    
+    private List<Person> _owners = new List<Person>();
+    public IEnumerable<Person> Owners => _owners;
+    
+    private static Dog Create(DogName name, DogAge age, DogBreed breed)
+    {
+        var dog = new Dog()
+        {
+            Name = name,
+            Age = age,
+            Breed = breed,
+        };
+
+        return dog;
+    }
+    
+    public void AddOwner(Person person)
+    {
+        _owners.Add(person);
+    }
+
+    public Unit Update(DogName name, DogAge age, DogBreed breed)
+    {
+        Name = name;
+        Age = age;
+        Breed = breed;
+        return Unit.Default;
+    }
+}
---
+++ PuppyPlace.Domain/Person.cs
@@ -0,0 +1,33 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using LanguageExt;
+using PuppyPlace.Domain.ValueObjects.PersonValueObjects;
+
+namespace PuppyPlace.Domain;
+
+public class Person
+{
+    public Guid Id { get; set; }
+    public PersonName Name { get; set; }
+    
+    private List<Dog> _dogs = new List<Dog>();
+    public IEnumerable<Dog> Dogs => _dogs;
+    
+    public Person(PersonName name)
+    {
+        Name = name;
+        Id = Guid.NewGuid();
+    }
+
+    public Unit Update(PersonName name)
+    {
+        Name = name;
+        return Unit.Default;
+    }
+
+    public void AdoptDog(Dog dog)
+    {
+        _dogs.Add(dog);
+    }
+    
+}
