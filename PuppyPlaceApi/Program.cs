d6ddc7ad48369fb58719e96cf50f0d9a7574eba0
tess-hartis <tess15hartis@gmail.com>
2022-02-20 22:09:13 -0600

    Update Cors policy in Api program file

--- PuppyPlaceApi/Program.cs
+++ PuppyPlaceApi/Program.cs
@@ -19,6 +19,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var specificcPolicy = "_specificcPolicy";
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy(name: specificcPolicy, policyBuilder =>
+    {
+        policyBuilder
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,7 +40,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseCors(specificcPolicy);
 
 app.UseAuthorization();
 
