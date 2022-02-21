8d7c0009e9194a9a8d14583f86d18fec6797cdc7
tess-hartis <tess15hartis@gmail.com>
2022-02-21 12:52:03 -0600

    Add GetPersonDto to shared project

---
+++ PuppyPlace.Shared/GetPersonDto.cs
@@ -0,0 +1,22 @@
+using PuppyPlace.Domain;
+
+namespace PuppyPlace.Shared;
+
+public class GetPersonDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public IEnumerable<string> Dogs { get; set; } = new List<string>();
+
+    public static GetPersonDto FromPerson(Person person)
+    {
+        var dogNames = person.Dogs.Select(x => x.Name.Value);
+
+        return new GetPersonDto
+        {
+            Id = person.Id,
+            Name = person.Name.Value,
+            Dogs = dogNames
+        };
+    }
+}
