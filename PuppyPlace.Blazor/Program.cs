a4f799746b43ee480d93edda4d216c1571e2255f
tess-hartis <tess15hartis@gmail.com>
2022-02-21 12:53:42 -0600

    Remove service from program file

--- PuppyPlace.Blazor/Program.cs
+++ PuppyPlace.Blazor/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PuppyPlace.Blazor;
-using PuppyPlace.Blazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,6 +9,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => 
     new HttpClient {BaseAddress = new Uri("https://localhost:7079/api/")});
 
-builder.Services.AddScoped<IPersonService, PersonService>();
 
 await builder.Build().RunAsync();
