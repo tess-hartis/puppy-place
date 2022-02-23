cca1e38d78edb4b03917ded0857880603c280a97
tess-hartis <tess15hartis@gmail.com>
2022-02-22 22:49:00 -0600

    Add DogDto and AddDogPage

---
+++ PuppyPlace.Blazor/Features/Dog/AddDogDto.cs
@@ -0,0 +1,9 @@
+namespace PuppyPlace.Blazor.Features.Dog;
+
+public class AddDogDto
+{
+    public string Name { get; set; } = "";
+    public string Age { get; set; } = "";
+    public string Breed { get; set; } = "";
+    
+}
