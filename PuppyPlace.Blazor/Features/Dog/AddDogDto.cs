431dba5c084f8f0e2479bd48515589888ea50dc9
tess-hartis <tess15hartis@gmail.com>
2022-02-23 22:55:21 -0600

    Add FluentValidation AddDogDto in Blazor project

--- PuppyPlace.Blazor/Features/Dog/AddDogDto.cs
+++ PuppyPlace.Blazor/Features/Dog/AddDogDto.cs
@@ -1,3 +1,5 @@
+using FluentValidation;
+
 namespace PuppyPlace.Blazor.Features.Dog;
 
 public class AddDogDto
@@ -5,5 +7,43 @@ public class AddDogDto
     public string Name { get; set; } = "";
     public string Age { get; set; } = "";
     public string Breed { get; set; } = "";
+}
+
+public class DogValidator : AbstractValidator<AddDogDto>
+{
+    public DogValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name cannot be empty")
+            .Length(2, 100).WithMessage("Invalid name length")
+            .Must(BeValidName).WithMessage("Name cannot contain special characters");;
+        RuleFor(x => x.Age)
+            .NotEmpty().WithMessage("Age cannot be empty")
+            .Must(BeValidInt).WithMessage("Invalid age");
+        RuleFor(x => x.Breed)
+            .NotEmpty().WithMessage("Breed cannot be empty")
+            .Length(2, 100).WithMessage("Invalid breed length")
+            .Must(BeValidName).WithMessage("Breed cannot contain special characters");;
+    }
     
+    private bool BeValidName(string name)
+    {
+        name = name.Replace(" ", "");
+        name = name.Replace("-", "");
+        return name.All(char.IsLetter);
+    }
+
+    private bool BeValidInt(string number)
+    {
+        if (!int.TryParse(number, out var parsed)) 
+            return false;
+        
+        if (parsed > 25 || parsed < 0)
+        {
+            return false;
+        }
+
+        return true;
+
+    }
 }
