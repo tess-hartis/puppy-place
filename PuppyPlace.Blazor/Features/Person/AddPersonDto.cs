25960c1417e7b2c3e7f79cf0fdc35dd3d8e3c360
tess-hartis <tess15hartis@gmail.com>
2022-02-23 22:56:00 -0600

    Add FluentValidation on AddPersonDto

--- PuppyPlace.Blazor/Features/Person/AddPersonDto.cs
+++ PuppyPlace.Blazor/Features/Person/AddPersonDto.cs
@@ -1,6 +1,26 @@
+using FluentValidation;
+
 namespace PuppyPlace.Blazor.Features.Person;
 
 public class AddPersonDto
 {
     public string Name { get; set; } = "";
+}
+
+public class PersonValidator : AbstractValidator<AddPersonDto>
+{
+    public PersonValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name cannot be empty")
+            .Length(2, 100).WithMessage("Invalid name length")
+            .Must(BeValidName).WithMessage("Name cannot contain special characters");
+    }
+
+    private bool BeValidName(string name)
+    {
+        name = name.Replace(" ", "");
+         name = name.Replace("-", "");
+         return name.All(char.IsLetter);
+    }
 }
