e25573600ffae6e9a86e750b3abd2beae8ff0079
tess-hartis <tess15hartis@gmail.com>
2022-02-24 20:49:26 -0600

    Add dto for adding or updating dog

---
+++ PuppyPlace.Blazor/Features/Dog/AddEditDogDto.cs
@@ -0,0 +1,10 @@
+using FluentValidation;
+
+namespace PuppyPlace.Blazor.Features.Dog;
+
+public class AddEditDogDto
+{
+    public string Name { get; set; } = "";
+    public string Age { get; set; } = "";
+    public string Breed { get; set; } = "";
+}
