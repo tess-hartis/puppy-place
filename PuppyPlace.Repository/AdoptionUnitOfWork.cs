0e9b5fc11c8550fd93fb9c23cdec919c6449830a
tess-hartis <tess15hartis@gmail.com>
2022-03-03 13:13:19 -0600

    Add Unit of Work for adopting dogs

---
+++ PuppyPlace.Repository/AdoptionUnitOfWork.cs
@@ -0,0 +1,43 @@
+using PuppyPlace.Data;
+
+namespace PuppyPlace.Repository;
+
+public interface IAdoptionUnitOfWork
+{
+    Task Save();
+    IPersonRepository PersonRepo { get; }
+    IDogRepository DogRepo { get; }
+}
+
+public class AdoptionUnitOfWork
+{
+    private readonly PuppyPlaceContext _context;
+    private IPersonRepository _personRepository;
+    private IDogRepository _dogRepository;
+
+    public AdoptionUnitOfWork(PuppyPlaceContext context)
+    {
+        _context = context;
+    }
+    
+    public IDogRepository DogRepo
+    {
+        get
+        {
+            return _dogRepository = new DogRepository(_context);
+        }
+    }
+
+    private IPersonRepository PersonRepo
+    {
+        get
+        {
+            return _personRepository = new PersonRepository(_context);
+        }
+    }
+
+    public async Task Save()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
