feb7c4421456126dbf8eabe34c956851e756bebe
tess-hartis <tess15hartis@gmail.com>
2022-03-03 13:39:30 -0600

    Add migration

---
+++ PuppyPlace.Data/Migrations/20220303193503_IdPropertyChange.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PuppyPlace.Data.Migrations
+{
+    public partial class IdPropertyChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
