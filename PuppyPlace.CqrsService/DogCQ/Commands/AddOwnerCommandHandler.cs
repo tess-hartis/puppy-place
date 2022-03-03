9b258daa182a2a72b4faf0e9af294b02bc8928ac
tess-hartis <tess15hartis@gmail.com>
2022-03-03 13:14:04 -0600

    Implement unit of work

--- PuppyPlace.CqrsService/DogCQ/Commands/AddOwnerCommandHandler.cs
+++ PuppyPlace.CqrsService/DogCQ/Commands/AddOwnerCommandHandler.cs
@@ -1,7 +1,6 @@
 using LanguageExt;
 using static LanguageExt.Prelude;
 using MediatR;
-using PuppyPlace.Data;
 using PuppyPlace.Repository;
 using Unit = LanguageExt.Unit;
 
@@ -18,45 +17,28 @@ public class AddOwnerCommand : IRequest<Option<Unit>>
         PersonId = personId;
     }
 }
+
 public class AddOwnerCommandHandler : IRequestHandler<AddOwnerCommand, Option<Unit>>
 {
-    private readonly PuppyPlaceContext _context;
-    private IDogRepository _dogRepository;
-    private IPersonRepository _personRepository;
+    private readonly IAdoptionUnitOfWork _unitOfWork;
 
-    public AddOwnerCommandHandler(PuppyPlaceContext context)
+    public AddOwnerCommandHandler(IAdoptionUnitOfWork unitOfWork)
     {
-        _context = context;
+        _unitOfWork = unitOfWork;
     }
     
-    private IDogRepository DogRepo
-    {
-        get
-        {
-            return _dogRepository = new DogRepository(_context);
-        }
-    }
-
-    private IPersonRepository PersonRepo
-    {
-        get
-        {
-            return _personRepository = new PersonRepository(_context);
-        }
-    }
-
     public async Task<Option<Unit>> Handle
         (AddOwnerCommand command, CancellationToken cancellationToken)
     {
-        var person = await PersonRepo.FindAsync(command.PersonId);
-        var dog = await DogRepo.FindAsync(command.DogId);
+        var person = await _unitOfWork.PersonRepo.FindAsync(command.PersonId);
+        var dog = await _unitOfWork.DogRepo.FindAsync(command.DogId);
 
         var result =
             from d in dog
             from p in person
             select d.AddOwner(p);
 
-        ignore(result.Map(async _ => await _context.SaveChangesAsync()));
+        ignore(result.Map(async _ => await _unitOfWork.Save()));
 
         return result;
     }
